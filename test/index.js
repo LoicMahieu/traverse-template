7d0593abcf74e6d83c214ddfeb556befdbaa037e
Loïc Mahieu <mahieuloic@gmail.com>
2016-07-07 18:10:43 +0200

    Initial commit

---
+++ test/index.js
@@ -0,0 +1,81 @@
+/*global describe, it*/
+
+const expect = require('chai').expect
+const traverseTemplate = require('..')
+
+describe('traverse-template', () => {
+  it('works', () => {
+    expect(
+      traverseTemplate({
+        vars: {
+          foo: 1,
+          nested: {
+            bar: 2
+          }
+        },
+        foo: '${vars.foo} !',
+        arr: [
+          {
+            foo: '${vars.foo}',
+            bar: [
+              '${vars.nested.bar}'
+            ]
+          },
+          '${vars.foo}'
+        ]
+      })
+    ).to.deep.equal({
+      vars: {
+        foo: 1,
+        nested: {
+          bar: 2
+        }
+      },
+      foo: '1 !',
+      arr: [
+        {
+          foo: '1',
+          bar: [
+            '2'
+          ]
+        },
+        '1'
+      ]
+    })
+  })
+
+  it('works with given locals', () => {
+    expect(
+      traverseTemplate({
+        foo: '${vars.foo} !',
+        arr: [
+          {
+            foo: '${vars.foo}',
+            bar: [
+              '${vars.nested.bar}'
+            ]
+          },
+          '${vars.foo}'
+        ]
+      }, {
+        vars: {
+          foo: 1,
+          nested: {
+            bar: 2
+          }
+        }
+      })
+    ).to.deep.equal({
+      foo: '1 !',
+      arr: [
+        {
+          foo: '1',
+          bar: [
+            '2'
+          ]
+        },
+        '1'
+      ]
+    })
+  })
+})
